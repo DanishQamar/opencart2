addca10c6c37a214b61063da48b1d3f0bd227b25
root <root@ip-172-31-39-87.ap-south-1.compute.internal>
2020-08-09 18:20:46 +0000

    dd

---
+++ upload/admin/config.php
@@ -0,0 +1,36 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://ec2-13-233-230-211.ap-south-1.compute.amazonaws.com/p/admin/');
+define('HTTP_CATALOG', 'http://ec2-13-233-230-211.ap-south-1.compute.amazonaws.com/p/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://ec2-13-233-230-211.ap-south-1.compute.amazonaws.com/p/admin/');
+define('HTTPS_CATALOG', 'http://ec2-13-233-230-211.ap-south-1.compute.amazonaws.com/p/');
+
+// DIR
+define('DIR_APPLICATION', '/var/www/opencart/upload/admin/');
+define('DIR_SYSTEM', '/var/www/opencart/upload/system/');
+define('DIR_IMAGE', '/var/www/opencart/upload/image/');
+define('DIR_STORAGE', '/var/www/opencart/storage/');
+define('DIR_CATALOG', '/var/www/opencart/upload/catalog/');
+define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
+define('DIR_TEMPLATE', DIR_APPLICATION . 'view/template/');
+define('DIR_CONFIG', DIR_SYSTEM . 'config/');
+define('DIR_CACHE', DIR_STORAGE . 'cache/');
+define('DIR_DOWNLOAD', DIR_STORAGE . 'download/');
+define('DIR_LOGS', DIR_STORAGE . 'logs/');
+define('DIR_MODIFICATION', DIR_STORAGE . 'modification/');
+define('DIR_SESSION', DIR_STORAGE . 'session/');
+define('DIR_UPLOAD', DIR_STORAGE . 'upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'vYFg4#@PDB');
+define('DB_DATABASE', 'opencart');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
+
+// OpenCart API
+define('OPENCART_SERVER', 'https://www.opencart.com/');
